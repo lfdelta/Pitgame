1fb7697c5c5434bb6994ab921bf8580e3d1f405a
Allison Kramer <allisonkramer@uchicago.edu>
2017-01-27 21:49:44 -0600

    Updating file structure

---
+++ Hookshot/Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+	public Transform topTrigger;
+	public Transform bottomTrigger;
+
+	private Transform player;
+	private bool playerGrounded;
+
+	void Start () {
+		// find and store the player transform	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+// snap to platforms
+// 
